6d2694b5fb73a2fe9683f90b8c950b30eefe521f
TAKAHASHI,Toru <torutk@gmail.com>
2016-07-05 09:03:17 +0900

    最小限のカレンダー表示プログラム
    * はてな日記2016-07-02で紹介したコード（ただし、JDK 8版）

---
+++ src/calendar/Calendar.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright © 2016 Toru Takahahshi. All rights reserved.
+ */
+package calendar;
+
+import com.sun.javafx.scene.control.skin.DatePickerSkin;
+import java.time.LocalDate;
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * カレンダーを表示するJavaFXアプリケーションクラス。
+ * 
+ */
+public class Calendar extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        DatePickerSkin datePickerSkin = new DatePickerSkin(new DatePicker(LocalDate.now()));
+        Node calendar = datePickerSkin.getPopupContent();
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(calendar);
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("Calendar");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args コマンドライン引数
+     */
+    public static void main(String[] args) {
+        System.out.println("Calendar program start");
+        launch(args);
+    }
+    
+}
