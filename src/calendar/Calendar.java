525390aeef63b777959c9118a1860c50660905a2
TAKAHASHI,Toru <torutk@gmail.com>
2016-07-08 00:13:23 +0900

    コマンドラインオプションで表示位置・大きさを指定可能とした。

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -5,7 +5,9 @@ package calendar;
 
 import com.sun.javafx.scene.control.skin.DatePickerSkin;
 import java.time.LocalDate;
+import java.util.Map;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
@@ -18,9 +20,12 @@ import javafx.stage.StageStyle;
  * 
  */
 public class Calendar extends Application {
+    private Stage stage;
     
     @Override
     public void start(Stage primaryStage) {
+        stage = primaryStage;
+        parseParameters();
         DatePickerSkin datePickerSkin = new DatePickerSkin(new DatePicker(LocalDate.now()));
         Node calendar = datePickerSkin.getPopupContent();
         
@@ -36,6 +41,19 @@ public class Calendar extends Application {
         primaryStage.show();
     }
 
+    /**
+     * コマンドラインオプション（名前付き値）を解析し、表示位置・大きさの指定があれば反映する。
+     */
+    private void parseParameters() {
+        Map<String, String> params = getParameters().getNamed();
+        Platform.runLater(() -> {
+            stage.setX(Double.valueOf(params.getOrDefault("x", "0.0")));
+            stage.setY(Double.valueOf(params.getOrDefault("y", "0.0")));
+            stage.setWidth(Double.valueOf(params.getOrDefault("width", "144")));
+            stage.setHeight(Double.valueOf(params.getOrDefault("height", "144")));
+        });
+    }
+    
     /**
      * @param args コマンドライン引数
      */
