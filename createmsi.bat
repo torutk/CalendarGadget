823f9755998cf23e375e8ec172d8685fc90b6071
TAKAHASHI,Toru <torutk@gmail.com>
2016-07-18 08:27:54 +0900

    MSIバージョンを0.1.8に変更

--- createmsi.bat
+++ createmsi.bat
@@ -6,7 +6,7 @@ javapackager -deploy -native msi ^
 -srcdir dist -srcfiles Calendar.jar ^
 -appclass calendar.Calendar ^
 -name "Calendar" ^
--BappVersion=0.1.6 ^
+-BappVersion=0.1.8 ^
 -title "Calendar" ^
 -vendor Takahashi ^
 -description "Calendar on desktop"
